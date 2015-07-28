ecc5587a92a7a6886d9b09b47a5793be52a25ff8
Brandon Mathis <bemathis@gmail.com>
2015-07-28 00:30:35 -0400

    tests

---
+++ test/test-creation.js
@@ -0,0 +1,35 @@
+/*global describe, beforeEach, it */
+'use strict';
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+
+describe('coffeegulpify generator', function () {
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        return done(err);
+      }
+
+      this.app = helpers.createGenerator('coffeegulpify', [
+        '../../app'
+      ]);
+      done();
+    }.bind(this));
+  });
+
+  it('creates expected files', function (done) {
+    var expected = [
+      '.jshintrc',
+      '.editorconfig'
+    ];
+
+    helpers.mockPrompt(this.app, {
+      'someOption': true
+    });
+    this.app.options['skip-install'] = true;
+    this.app.run({}, function () {
+      helpers.assertFile(expected);
+      done();
+    });
+  });
+});
